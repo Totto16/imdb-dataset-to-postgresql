376a123b503796dfdb6b10dcf0bff829bfd72c2a
Totto16 <tobiausgais@gmail.com>
2024-06-08 22:35:31 +0200

    add tl-expected as fallback fro std::expected

---
+++ src/helper/expected.hpp
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#ifdef _USE_TL_EXPECTED
+#include <tl/expected.hpp>
+#else
+#include <expected>
+#endif
+
+namespace helper {
+
+#ifdef _USE_TL_EXPECTED
+template <typename S, typename T> using expected = tl::expected<S, T>;
+
+template <typename T> using unexpected = tl::unexpected<T>;
+
+#else
+template <typename S, typename T> using expected = std::expected<S, T>;
+
+template <typename T> using unexpected = std::unexpected<T>;
+
+#endif
+
+} // namespace helper
