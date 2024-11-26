90c3d63a62494747ae73d2580cff78af22657b95
Totto16 <tobiausgais@gmail.com>
2024-11-26 10:14:46 +0100

    chore: add clear tables sql script

---
+++ scripts/clear_tables.sql
@@ -0,0 +1,11 @@
+
+-- CLEAR(TRUNCATE) DATABASE imdb
+
+--delete tables, in correct order
+TRUNCATE TABLE public.title_ratings CASCADE;
+TRUNCATE TABLE public.title_principals CASCADE;
+TRUNCATE TABLE public.title_episode CASCADE;
+TRUNCATE TABLE public.title_crew CASCADE;
+TRUNCATE TABLE public.title_akas CASCADE;
+TRUNCATE TABLE public.name_basics CASCADE ;
+TRUNCATE TABLE public.title_basics CASCADE;
